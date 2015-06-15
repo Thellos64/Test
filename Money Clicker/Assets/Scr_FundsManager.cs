a50628b8ac6ac946420509a7476462873c46c8c6
Robert Billings <thellos64@gmail.com>
2015-06-15 12:27:17 +0100

    Added game
    This cool

---
+++ Money Clicker/Assets/Scr_FundsManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Scr_FundsManager : MonoBehaviour {
+
+	public GameObject SourceOfFunds; //Requests increase or decrease of funds
+	float DeltaFunds = 0; //The change of funds on this frame 
+	float CurrentFunds = 0; //The current bank of funds
+
+	public Text TextObject;
+
+
+	// Use this for initialization
+	void Start () {
+		UpdateText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (DeltaFunds != 0) { //If DeltaFunds has an amount, add it to CurrentFunds
+			CurrentFunds += DeltaFunds;
+			DeltaFunds = 0; //set DeltaFunds to 0 once it has been added
+			UpdateText();
+		}
+	}
+
+	public void IncreaseFunds(float Fundflow){ //Increse DeltaFunds by the amount of FundFlow
+		DeltaFunds += Fundflow;
+	}
+
+	public void DecreaseFunds (float FundFlow){//Decrese DeltaFunds by the amount of FundFlow
+		if (CurrentFunds >= FundFlow) {
+			DeltaFunds -= FundFlow;
+		}
+	}
+
+	void UpdateText(){ 					//Update Text to display current funds
+		TextObject.text = CurrentFunds.ToString();
+	}
+}
---
+++ Money Clicker/Assets/Scr_MoneyTree.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Scr_MoneyTree : MonoBehaviour {
+
+	public Text Text_FundAmount;
+	Scr_FundsManager BankAccountManager;
+
+	public int NumOfTrees = 0;
+
+	float Revenue;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("OneSecond", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OneSecond () { //Every second do this stuff
+
+		BankAccountManager = Text_FundAmount.GetComponent<Scr_FundsManager> ();
+
+		Revenue = NumOfTrees;
+
+		BankAccountManager.IncreaseFunds(Revenue);
+	}
+
+	public void IncrementNumOfTress() {
+		NumOfTrees ++;
+	}
+}
